e05dc366eb30ccc00def297456b744fdbfe09a11
abdurDaily <abdurdiary@gmail.com>
2024-03-26 04:05:12 -0700

    header

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('#banner').slick({
+        dots: true,
+        dotsClass: 'container test',
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+    });
+})
